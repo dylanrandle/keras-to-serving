6436a9c3ebe1d1971880111f8cecdc69d5c64058
Dylan Randle <jacquielabatt@Dylans-MacBook.local>
2017-09-06 22:48:36 -0400

    clean  up

--- export.py
+++ export.py
@@ -1,5 +1,4 @@
 import argparse
-import os
 
 import keras.backend as K
 from keras.models import load_model
@@ -11,7 +10,7 @@ def load_inference_model(model_path):
 def main():
 	parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 	parser.add_argument('--model_path', type=str, help='path to saved Keras model')
-	parser.add_argument('--export_path', type=str, default='./servables/tf_serving_model', help='path to Tensorflow servable')
+	parser.add_argument('--export_path', type=str, default='servables/tf_servable', help='path to Tensorflow servable')
 	parser.add_argument('--export_version', type=int, help='model version (integer)')
 	args = parser.parse_args()
 	model = load_inference_model(args.model_path)
@@ -19,10 +18,8 @@ def main():
 
 def export(model, args):
 	from tensorflow.python.saved_model import builder as saved_model_builder
-	from tensorflow.python.saved_model import utils
-	from tensorflow.python.saved_model import tag_constants, signature_constants
-	from tensorflow.python.saved_model.signature_def_utils_impl import build_signature_def, predict_signature_def
-	from tensorflow.contrib.session_bundle import exporter
+	from tensorflow.python.saved_model import tag_constants
+	from tensorflow.python.saved_model.signature_def_utils_impl import predict_signature_def
 
 	builder = saved_model_builder.SavedModelBuilder(args.export_path+'/'+args.export_version)
 	signature = predict_signature_def(inputs={'inputs': model.input}, outputs={'ouputs': model.output})
